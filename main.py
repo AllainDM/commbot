909fea85621bda656112757c404fcd9ca43655d9
Allain <allaindemoupu@mail.ru>
2023-04-08 19:38:29 +0300

    Рабочий вариант добавления подключений

--- main.py
+++ main.py
@@ -40,6 +40,10 @@ data = {
 }
 response = session.post(url_login, data=data, headers=HEADERS).text
 
+# Наши улицы в "совместных" районах
+moscow = [" Смоленская ул.", " Киевская ул."]
+frunze = [" Тосина ул.", " Тамбовская ул."]
+
 
 @dp.message_handler()  # commands=['Кировский']
 async def echo_mess(message: types.Message):
@@ -96,9 +100,14 @@ async def echo_mess(message: types.Message):
         await bot.send_message(message.chat.id, f"Ответ: тестим ексель")
         exel = open("example.xls", "rb")
         await bot.send_document(message.chat.id, exel)
+    elif message.text == "000" or message.text == "улицы":
+        await bot.send_message(message.chat.id, f"Наши улицы в московском районе: {moscow}")
+        await bot.send_message(message.chat.id, f"Наши улицы во фрунзенском районе: {frunze}")
     elif message.text == "0" or message.text == "тест":
         await bot.send_message(message.chat.id, f"Ответ: тестим ексель")
         answer = get_html_users(url.url_link_test)
+        exel = open("example.xls", "rb")
+        await bot.send_document(message.chat.id, exel)
         # answer2 = get_html_users(url.url_link_test2)
     else:
         help = "1: Кировский, 2: Адмирал, 3: Центр, 4: Парфеновская, 5: Измайловский, 6: Фрунзенский, 7: Малая М, " \
@@ -199,9 +208,7 @@ def get_html(url2):
 
         answer.reverse()
 
-        # print(list_repairs_id)
         return answer
-        # return "ok"
     else:
         print("error")
 
@@ -213,12 +220,12 @@ def get_html_users(url3):
         answer = ["Больше ничего нету"]  # Ответ боту
         list_users = []  # Тут храним что-то
         if html.status_code == 200:
-            # soup = BeautifulSoup(html.text, 'lxml')
-            # table = soup.find_all('tr', class_="cursor_pointer")
-
-            # print(table)
+            soup = BeautifulSoup(html.text, 'lxml')
+            table = soup.find_all('tr', class_="cursor_pointer")
+            test_save(table)
+            # print(table[0].text)
+            # print(type(table[0].text))
             return answer
-            # return "ok"
         else:
             print("error")
     except requests.exceptions.TooManyRedirects as e:
@@ -232,33 +239,109 @@ def get_one_comment(url1):
         soup = BeautifulSoup(html.text, 'lxml')
         soup = soup.get_text()
         soup = soup.strip()
-        # print(soup)
         return soup
     return "no comment"
 
 
 def test_save(table):
-    # font0 = xlwt.Font()
-    # font0.name = 'Times New Roman'
-    # font0.colour_index = 2
-    # font0.bold = True
-
-    # style0 = xlwt.XFStyle()
-    # style0.font = font0
-
-    # style1 = xlwt.XFStyle()
-    # style1.num_format_str = 'D-MMM-YY'
-
+    # table = table.reverse()
+    # Для разворота можно все сделать в виде списка внутри списка, который и развернуть
+    table_list = []
     wb = xlwt.Workbook()
     ws = wb.add_sheet('A Test Sheet')
+    num_string = 1  # Стартовый номер строки для екселя
+    for i in table:
+        one_list = []
 
-    ws.write(0, 0, 'Test')
-    # ws.write(0, 0, 'Test', style0)
-    # ws.write(1, 0, datetime.now(), style1)
-    # ws.write(1, 0, datetime.now(), style1)
-    ws.write(2, 0, 1)
-    ws.write(2, 1, 1)
-    ws.write(2, 2, xlwt.Formula("A3+B3"))
+        # Нужно найти элемент с фамилией мастера и номер договора.
+        # 2: мастер, 3: номер договора
+        td_class_all = i.find_all('td', class_="")
+        # print(td_class_all[2].text)
+        # print(td_class_all[3].text)
+        pact = td_class_all[3].text
+        soname = td_class_all[2].text
+        soname = soname.split()
+        # Без фамилии Тиера, а она нужна
+        if not soname:
+            soname = [" "]
+        print(soname[0])
+
+        # Тут должна быть дата
+        td_class_div_center = i.find_all('td', class_="div_center")
+        # По длине даты можно отсортировать лишние задания
+        date = td_class_div_center[-1].text
+        date = date.split()
+        if not date:
+            continue
+        elif len(date[0]) == 10:
+            print(f'Длинна в норме {date}: {len(date[0])} ')
+        else:
+            continue
+
+        # В ссылках хранится адрес, ищем ссылки
+        list_a = i.find_all('a')  # Ищем ссылки во всей таблице
+        address = list_a[2].text
+        print(address)
+        address = address.split(",")
+        print(address)
+        # Отдельно надо разделить номер дома и квартиру
+        # print(address)
+        address_dom = address[4].split()
+        # print(address_dom[0])
+        address_kv = address[-1].split()
+        # print(address_kv[-1])
+
+        # Вычеркнем лишние улицы из "совместных" районов
+        # Нужен хелп по улицам, ответ бота при запросе. Сделать улицы переменными, может в массиве
+        if " Московский р-н" in address:
+            for street in moscow:
+                if street in address:
+                    print(f"Улица {address[3][0:-4]} находится в Московском районе")
+                else:
+                    break
+        if " Фрунзенский р-н" in address:
+            for street in frunze:
+                if street in address:
+                    print(f"Улица {address[3][0:-4]} находится во Фрунзенском районе")
+                else:
+                    break
+        # else:
+        #     print(f"Улица {address[3][0:-4]} НЕ находится в Московском районе?")
+
+        one_list.append("Бренд")  # Дата
+        one_list.append(date)  # Дата
+        one_list.append(address[3][0:-4])  # Улица
+        one_list.append(pact)   # Номер договора
+        one_list.append(address_dom[0])  # Дом
+        one_list.append(address_kv[-1])  # Квартира
+        one_list.append(soname[0])  # Мастер
+        one_list.append(address[2][0:-4])  # Район
+
+        table_list.append(one_list)
+
+    # Пока не переворачиваем, чтоб удобнее сравнивать
+    # table_list.reverse()
+    for i in table_list:
+        ws.write(num_string, 1, i[1])  # Дата
+        ws.write(num_string, 2, i[2])  # Номер договора
+        ws.write(num_string, 3, i[3])  # Улица 9
+        ws.write(num_string, 4, i[4])  # Дом
+        ws.write(num_string, 5, i[5])  # Квартира
+        ws.write(num_string, 6, i[6])  # Мастер
+        ws.write(num_string, 7, i[7])  # Район
+        num_string += 1
+
+        # ws.write(num_string, 1, date)  # Дата 18
+        # # ws.write(num_string, 1, str[18])  # Дата 18
+        # # ws.write(num_string, 2, str[17])  # Номер договора 17
+        # ws.write(num_string, 3, address[3][0:-4])  # Улица 9
+        # ws.write(num_string, 4, address_dom[0])  # Дом 11
+        # ws.write(num_string, 5, address_kv[-1])  # Квартира 13
+        # # ws.write(num_string, 6, str[14])  # Мастер 14
+        # ws.write(num_string, 7, address[2][0:-4])  # Район 7
+        # num_string += 1
+
+    # ws.write(2, 2, xlwt.Formula("A3+B3"))
 
     wb.save('example.xls')
 
